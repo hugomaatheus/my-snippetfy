d72d5a5be13e3214ae2389419ee729338c1909c9
hugomatheuss <hugomatheus650@gmail.com>
2018-10-23 13:12:06 -0300

    sequelize, nodemon, nunjucks e etc

---
+++ config/database.js
@@ -0,0 +1,7 @@
+module.exports = {
+  username: 'postgres',
+  password: 'batman123',
+  database: 'snippetfy',
+  host: '127.0.0.1',
+  dialect: 'pgsql',
+};
---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const nunjucks = require('nunjucks');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+nunjucks.configure(path.resolve('app', 'views'), {
+  autoescape: true,
+  express: app,
+});
+
+app.set('view engine', 'njk');
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(3000, () => {
+  console.log('Yo, app listening on port 3000!');
+});
