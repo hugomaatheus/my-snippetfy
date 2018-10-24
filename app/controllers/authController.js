25010724b262b00a03eaf7eb02b2fe9012d0b628
hugomatheuss <hugomatheus650@gmail.com>
2018-10-24 14:48:43 -0300

    mo' middlewares

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -10,38 +10,46 @@ module.exports = {
     return res.render('auth/signup');
   },
 
-  async register(req, res) {
-    const { email } = req.body;
+  async register(req, res, next) {
+    try {
+      const { email } = req.body;
 
-    if (await User.findOne({ where: { email } })) {
-      req.flash('error', 'Ooops, e-mail já cadastrado!');
-      return res.redirect('back');
+      if (await User.findOne({ where: { email } })) {
+        req.flash('error', 'Ooops, e-mail já cadastrado!');
+        return res.redirect('back');
+      }
+      const password = await bcrypt.hash(req.body.password, 5);
+      await User.create({ ...req.body, password });
+
+      req.flash('success', 'Usuário cadastrado com sucesso!');
+      return res.redirect('/');
+    } catch (err) {
+      return next(err);
     }
-    const password = await bcrypt.hash(req.body.password, 5);
-    await User.create({ ...req.body, password });
-
-    req.flash('success', 'Usuário cadastrado com sucesso!');
-    return res.redirect('/');
   },
 
-  async authenticate(req, res) {
-    const { email, password } = req.body;
-    const user = await User.findOne({ where: { email } });
+  async authenticate(req, res, next) {
+    try {
+      const { email, password } = req.body;
+      const user = await User.findOne({ where: { email } });
 
-    if (!user) {
-      req.flash('error', 'iih, usuário inexistente');
-      return res.redirect('back');
+      if (!user) {
+        req.flash('error', 'iih, usuário inexistente');
+        return res.redirect('back');
+      }
+
+      if (!await bcrypt.compare(password, user.password)) {
+        req.flash('error', 'opsss, senha incorreta cara!');
+        return res.redirect('back');
+      }
+
+      req.session.user = user;
+      return req.session.save(() => {
+        res.redirect('app/dashboard');
+      });
+    } catch (err) {
+      return next(err);
     }
-
-    if (!await bcrypt.compare(password, user.password)) {
-      req.flash('error', 'opsss, senha incorreta cara!');
-      return res.redirect('back');
-    }
-
-    req.session.user = user;
-    return req.session.save(() => {
-      res.redirect('app/dashboard');
-    });
   },
 
   signout(req, res) {
--- app/routes.js
+++ app/routes.js
@@ -3,6 +3,7 @@ const express = require('express');
 const routes = express.Router();
 
 const authMiddleware = require('./middlewares/auth');
+const guestMiddleware = require('./middlewares/guest');
 const authController = require('./controllers/authController');
 const dashboardController = require('./controllers/dashboardController');
 
@@ -12,8 +13,8 @@ routes.use((req, res, next) => {
   next();
 });
 
-routes.get('/', authController.signin);
-routes.get('/signup', authController.signup);
+routes.get('/', guestMiddleware, authController.signin);
+routes.get('/signup', guestMiddleware, authController.signup);
 routes.get('/signout', authController.signout);
 
 routes.post('/register', authController.register);
@@ -21,5 +22,16 @@ routes.post('/authenticate', authController.authenticate);
 
 routes.use('/app', authMiddleware);
 routes.get('/app/dashboard', dashboardController.index);
+
+routes.use((req, res) => res.render('errors/404'));
+
+routes.use((err, req, res, _next) => {
+  res.status(err.status || 500);
+
+  return res.render('errors/index', {
+    message: err.message,
+    error: process.env.NODE_ENV === 'production' ? {} : err,
+  });
+});
 
 module.exports = routes;
