78b2a56c3cc8dab3b9d7c553a344b3baa6cd6b2f
hugomatheuss <hugomatheus650@gmail.com>
2018-10-24 11:00:19 -0300

    bcrypt

--- app/routes.js
+++ app/routes.js
@@ -2,9 +2,11 @@ const express = require('express');
 
 const routes = express.Router();
 
-const userController = require('./controllers/authController');
+const authController = require('./controllers/authController');
 
-routes.get('/', userController.signin);
-routes.get('/signup', userController.signup);
+routes.get('/', authController.signin);
+routes.get('/signup', authController.signup);
+
+routes.post('/register', authController.register);
 
 module.exports = routes;
