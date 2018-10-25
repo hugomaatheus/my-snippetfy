60094a078e60299c9af2e90619ad52af025db463
hugomatheuss <hugomatheus650@gmail.com>
2018-10-25 17:42:09 -0300

    remove method

--- app/controllers/snippetController.js
+++ app/controllers/snippetController.js
@@ -44,4 +44,26 @@ module.exports = {
       return next(err);
     }
   },
+  async update(req, res, next) {
+    try {
+      const snippet = await Snippet.findById(req.params.id);
+
+      await snippet.update(req.body);
+
+      req.flash('success', 'Snippet atualizado com sucesso!');
+      return res.redirect(`/app/categories/${req.params.categoryId}/snippets/${snippet.id}`);
+    } catch (err) {
+      return next(err);
+    }
+  },
+  async destroy(req, res, next) {
+    try {
+      await Snippet.destroy({ where: { id: req.params.id } });
+
+      req.flash('success', 'Snippet deletado com sucesso!');
+      return res.redirect(`/app/categories/${req.params.categoryId}`);
+    } catch (err) {
+      return next(err);
+    }
+  }
 };
