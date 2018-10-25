dc3ff0a81c3c9e5870bf9555678eddea5577eee7
hugomatheuss <hugomatheus650@gmail.com>
2018-10-25 16:41:57 -0300

    ooh yeah

--- app/controllers/snippetController.js
+++ app/controllers/snippetController.js
@@ -1,4 +1,4 @@
-const { Snippet } = require('../models');
+const { Snippet, Category } = require('../models');
 
 module.exports = {
   async store(req, res, next) {
@@ -13,6 +13,33 @@ module.exports = {
       req.flash('success', 'Snippet criada com sucesso!');
 
       return res.redirect(`/app/categories/${req.params.categoryId}/snippets/${snippet.id}`);
+    } catch (err) {
+      return next(err);
+    }
+  },
+
+  async show(req, res, next) {
+    try {
+      const { categoryId, id } = req.params;
+
+      const categories = await Category.findAll({
+        include: [Snippet],
+        where: {
+          UserId: req.session.user.id,
+        },
+      });
+
+      const snippets = await Snippet.findAll({
+        where: { CategoryId: categoryId },
+      });
+
+      const snippet = await Snippet.findById(id);
+      res.render('snippets/show', {
+        activeCategory: categoryId,
+        categories,
+        snippets,
+        currentSnippet: snippet,
+      });
     } catch (err) {
       return next(err);
     }
